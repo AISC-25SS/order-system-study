05e41d685de808f1c8e00370e88259ba2e2fa7d0
dowon <kd1412@hs.ac.kr>
2025-05-20 00:13:58 +0900

    [feat] #2 domain(id.가격,이름) 구현 및 등록 setter/getter로 구현

--- src/item/Item.java
+++ src/item/Item.java
@@ -5,6 +5,7 @@ public class Item {
         private String name;  // 상품명
         private int price;    // 가격 (원)
 
+
         // 모든 필드를 초기화하는 생성자
         public Item(Long id, String name, int price) {
             this.id = id;
