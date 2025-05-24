af2a55f9692cc6b024ce12853c6b0faac8ab9636
Binz <singriet0824@hs.ac.kr>
2025-05-24 14:12:59 +0900

    refactor : 할인 시스템 구현
    resolves: #14

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.*;
+import discount.FixedDiscountPolicy;
+import discount.RateDiscountPolicy;
+import tempMember.Grade;
+import tempMember.Member;
+
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello Git Seminar");
+
+        int discount;
+        int price;
+        Scanner sc = new Scanner(System.in);
+        price = sc.nextInt();
+
+
+        Member member1 = new Member(1L, "bin", "qwer1234","종빈", Grade.VIP);
+
+        RateDiscountPolicy totalPrice= new RateDiscountPolicy();
+        discount = totalPrice.discount(member1, price);
+        System.out.println("할인된 가격:" + discount);
+
+
+
     }
 }
--- src/discount/RateDiscountPolicy.java
+++ src/discount/RateDiscountPolicy.java
@@ -6,13 +6,18 @@ import tempMember.Member;
 // RateDiscountPolicy.java
 public class RateDiscountPolicy implements DiscountPolicy {
 
-    private final int discountPercent = 10; // 10% 할인
+    private final int discountPercent = 10;// 10% 할인
+
+
+    public RateDiscountPolicy() {
+
+    }
 
     @Override
     public int discount(Member member, int price) {
         // price > 100 이라는 가정 하에 구현했습니다
         if (member.getGrade() == Grade.VIP)
-            return (100 - discountPercent) * (price / 100);
+            return (int)Math.round(price * (discountPercent /100.0));
         else
             return price;
     }
