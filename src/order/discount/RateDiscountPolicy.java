4c08fa643faf40583baab80e72754948fe6f2b09
Binz <singriet0824@hs.ac.kr>
2025-05-26 20:52:38 +0900

    refactor : 할인 시스템 리턴값 수정
    할인 금액이라는 용어가 깎아줄 금액을 말하는건지 아니면 할인이 적용된 가격인건지 모호했는데 전자로 통일했습니다
    
    resolves: #14

--- src/order/discount/RateDiscountPolicy.java
+++ src/order/discount/RateDiscountPolicy.java
@@ -1,4 +1,4 @@
-package discount;
+package order.discount;
 
 import tempMember.Grade;
 import tempMember.Member;
@@ -9,16 +9,12 @@ public class RateDiscountPolicy implements DiscountPolicy {
     private final int discountPercent = 10;// 10% 할인
 
 
-    public RateDiscountPolicy() {
-
-    }
-
     @Override
     public int discount(Member member, int price) {
-        // price > 100 이라는 가정 하에 구현했습니다
+
         if (member.getGrade() == Grade.VIP)
             return (int)Math.round(price * (discountPercent /100.0));
         else
-            return price;
+            return 0;
     }
 }
