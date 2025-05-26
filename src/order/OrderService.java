9821149120aa02b451f2505598166c4dc5a32792
Fwe1 <huhjhace@gmail.com>
2025-05-26 20:50:37 +0900

    feat : OrderService 구현

---
+++ src/order/OrderService.java
@@ -0,0 +1,12 @@
+package order;
+
+import java.util.List;
+
+public interface OrderService { //계산서 조회 인터페이스
+    Order registerOrder(Order order); //Order객체 등록
+    void ListOrders(); 	//모든 Order객체 열거
+    List<Order> getALLOrders(); //모든 Order객체 열거
+    List<Order> getOrder(); //Order객체 찾기
+    boolean updateOrder(Order order); //업데이트가 됐다면 true, 없으면 false리턴
+    boolean deleteOrder(Order order); //삭제가 됐다면 true, 없으면 false 리턴
+}
